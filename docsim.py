c606161379c00d4b2532ef4ec911854a8464fc20
Carl Jackson <carl.jackson@gmail.com>
2017-11-06 10:11:31 -0500

    Working cluster analysis – still messy

--- docsim.py
+++ docsim.py
@@ -1,27 +1,43 @@
-# Things to do:
-
-# Read in files
-# Parse files: split into words
-# Find word obscurity and score them
-# Find out how many words they have in common
-
-import spacy, textract, os, re
+import spacy, textract, os, re, math
 import numpy as np
+import pandas as pd
 from collections import Counter
-import math
 from textblob import TextBlob as tb
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 
-# Cluster algorithm
-def cluster(tfidf_matrix):
 
-    num_clusters = 10
+def tokenize_only(text):
+
+    #print(text)
+
+    tokens = [token.text for token in text if token.is_stop != True
+                         and token.is_punct != True]
+
+    #print(tokens)
+    #print(tokens[0])
+    #print(len(text))
+
+    # first tokenize by sentence, then by word to ensure that punctuation is caught as it's own token
+    #tokens = [word.lower() for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
+    filtered_tokens = []
+    # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
+    for token in tokens:
+        #if re.search('[a-zA-Z]', token):
+        filtered_tokens.append(token.lower())
+
+    #print(filtered_tokens[0])
+
+    return filtered_tokens
+
+# Cluster algorithm
+def cluster(tfidf_matrix,num_clusters):
+
     km = KMeans(n_clusters=num_clusters)
     km.fit(tfidf_matrix)
     clusters = km.labels_.tolist()
 
-    print(clusters)
+    return [clusters,km]
 
 # Textblob functions
 def tf(word, blob):
@@ -76,13 +92,6 @@ def extract_title(text_list):
 # Build similarity matrix
 def build_sim_matrix(text,method):
 
-    #words = [[token.text for token in row if token.is_stop != True
-                        #and token.is_punct != True] for row in text]
-
-    #print(words)
-    #print(words[0])
-    #print(len(text))
-
     sim_mat = np.zeros((len(text), len(text)))
 
     if method == 'spacy':
@@ -101,12 +110,14 @@ def build_sim_matrix(text,method):
                 tfidf = vect.fit_transform(text)
                 sim_mat = (tfidf * tfidf.T).A
 
-    return tfidf
+    terms = vect.get_feature_names()
+
+    return [tfidf,terms,sim_mat]
 
 
 if __name__ == '__main__':
 
-    # do some stuffs!
+    # Load nlp and set path
     nlp = spacy.load('en', parser=False)
     path = '/Users/Carl/Documents/9thCo/Articles/'
 
@@ -122,12 +133,27 @@ if __name__ == '__main__':
     texts = extract_text(path,'TF-IDF')
     texts,titles = extract_title(texts)
 
-    #texts = extract_text(path,'spacy')
+    spacy_texts = extract_text(path,'spacy')
 
     # Build similarity matrix
-    tfidf = build_sim_matrix(texts,'TF-IDF')
+    [tfidf,terms,sim_mat] = build_sim_matrix(texts,'TF-IDF')
     #similarity_matrix2 = build_sim_matrix(texts,'spacy')
 
+    # Tokens
+    #tokenize_only(spacy_texts[0])
+
+    totalvocab_tokenized = []
+    for i in spacy_texts:
+        #allwords_stemmed = tokenize_and_stem(i) #for each item in 'synopses', tokenize/stem
+        #totalvocab_stemmed.extend(allwords_stemmed) #extend the 'totalvocab_stemmed' list
+
+        allwords_tokenized = tokenize_only(i)
+        totalvocab_tokenized.extend(allwords_tokenized)
+
+    vocab_frame = pd.DataFrame({'words': totalvocab_tokenized})
+    print('there are ' + str(vocab_frame.shape[0]) + ' items in vocab_frame')
+
+    #print(vocab_frame.iloc[4147])
     #dist_matrix = 1-similarity_matrix
 
     #print(dist_matrix)
@@ -135,5 +161,36 @@ if __name__ == '__main__':
     #print(max(similarity_matrix[0,1:]))
     #print(1-max(similarity_matrix2[0,1:]))
 
-    # Cluster
-    cluster(tfidf)
+    # Cluster analysis
+    num_clusters = 10
+    [clusters,km] = cluster(tfidf,num_clusters)
+    articles = { 'title': titles, 'cluster': clusters }
+    frame = pd.DataFrame(articles, index = [clusters] , columns = ['title', 'cluster'])
+    print("Number of items per cluster: ")
+    print(frame['cluster'].value_counts())
+
+    terms_per_cluster = 6
+
+    print("Top terms per cluster:")
+    print()
+    #sort cluster centers by proximity to centroid
+    order_centroids = km.cluster_centers_.argsort()[:, ::-1]
+
+    for i in range(num_clusters):
+        print("Cluster %d words: " % i, end='')
+
+        for ind in order_centroids[i, :terms_per_cluster]: #replace 6 with n words per cluster
+            #print(terms[ind].split(' '))
+            print(terms[ind], end=' ')
+            #print(' %s' % vocab_frame.ix[terms[ind].split(' ')].values.tolist()[0][0], end=',')
+        print() #add whitespace
+        print() #add whitespace
+
+        print("Cluster %d titles:" % i, end='')
+        for title in frame.ix[i]['title'].values.tolist():
+            print(' %s,' % title, end='')
+        print() #add whitespace
+        print() #add whitespace
+
+    print()
+    print()
