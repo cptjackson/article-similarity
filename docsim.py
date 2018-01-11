5055d22b7b1a76920ae178c6127d68564c64ca6b
Carl Jackson <carl.jackson@gmail.com>
2018-01-11 13:19:31 -0500

    Ensure that diagram is displayed when running.

--- docsim.py
+++ docsim.py
@@ -4,10 +4,107 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 from sklearn.metrics.pairwise import cosine_similarity
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt, mpld3
 import matplotlib as mpl
 from sklearn.manifold import MDS
 
+
+#define custom toolbar location
+class TopToolbar(mpld3.plugins.PluginBase):
+    """Plugin for moving toolbar to top of figure"""
+
+    JAVASCRIPT = """
+    mpld3.register_plugin("toptoolbar", TopToolbar);
+    TopToolbar.prototype = Object.create(mpld3.Plugin.prototype);
+    TopToolbar.prototype.constructor = TopToolbar;
+    function TopToolbar(fig, props){
+        mpld3.Plugin.call(this, fig, props);
+    };
+
+    TopToolbar.prototype.draw = function(){
+      // the toolbar svg doesn't exist
+      // yet, so first draw it
+      this.fig.toolbar.draw();
+
+      // then change the y position to be
+      // at the top of the figure
+      this.fig.toolbar.toolbar.attr("x", 150);
+      this.fig.toolbar.toolbar.attr("y", 400);
+
+      // then remove the draw function,
+      // so that it is not called again
+      this.fig.toolbar.draw = function() {}
+    }
+    """
+    def __init__(self):
+        self.dict_ = {"type": "toptoolbar"}
+
+
+def plot_web_stuff(xs,ys,clusters,titles,labels):
+
+    #set up colors per clusters using a dict
+    cluster_colors = {0: '#1b9e77', 1: '#d95f02', 2: '#7570b3', 3: '#e7298a', 4: '#66a61e'}
+
+    #set up cluster names using a dict
+    cluster_names = {}
+    for i in range(5):
+        cluster_names[i] = labels[i]
+
+    #create data frame that has the result of the MDS plus the cluster numbers and titles
+    df = pd.DataFrame(dict(x=xs, y=ys, label=clusters, title=titles))
+
+    #group by cluster
+    groups = df.groupby('label')
+
+    #define custom css to format the font and to remove the axis labeling
+    css = """
+    text.mpld3-text, div.mpld3-tooltip {
+      font-family:Arial, Helvetica, sans-serif;
+    }
+
+    g.mpld3-xaxis, g.mpld3-yaxis {
+    display: none; }
+
+    svg.mpld3-figure {
+    margin-left: -200px;}
+    """
+
+    # Plot
+    fig, ax = plt.subplots(figsize=(14,6)) #set plot size
+    ax.margins(0.03) # Optional, just adds 5% padding to the autoscaling
+
+    #iterate through groups to layer the plot
+    #note that I use the cluster_name and cluster_color dicts with the 'name' lookup to return the appropriate color/label
+    for name, group in groups:
+        points = ax.plot(group.x, group.y, marker='o', linestyle='', ms=18,
+                         label=cluster_names[name], mec='none',
+                         color=cluster_colors[name])
+        ax.set_aspect('auto')
+        labels = [i for i in group.title]
+
+        #set tooltip using points, labels and the already defined 'css'
+        tooltip = mpld3.plugins.PointHTMLTooltip(points[0], labels,
+                                           voffset=10, hoffset=10, css=css)
+        #connect tooltip to fig
+        mpld3.plugins.connect(fig, tooltip, TopToolbar())
+
+        #set tick marks as blank
+        ax.axes.get_xaxis().set_ticks([])
+        ax.axes.get_yaxis().set_ticks([])
+
+        #set axis as blank
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+
+
+    ax.legend(numpoints=1) #show legend with only one dot
+
+    mpld3.show() #show the plot
+    #mpld3.display() #use this for jupyter notebook
+
+    #uncomment the below to export to html
+    #html = mpld3.fig_to_html(fig)
+    #print(html)
 
 def plot_stuff(xs,ys,clusters,titles,labels):
 
@@ -258,4 +355,25 @@ if __name__ == '__main__':
     print()
     print()
 
-    plot_stuff(xs,ys,clusters,titles,labels)
+    #plot_stuff(xs,ys,clusters,titles,labels)
+    plot_web_stuff(xs,ys,clusters,titles,labels)
+
+    # Hierarchy
+    from scipy.cluster.hierarchy import ward, dendrogram
+
+    linkage_matrix = ward(dist) #define the linkage_matrix using ward clustering pre-computed distances
+
+    fig, ax = plt.subplots(figsize=(15, 20)) # set size
+    ax = dendrogram(linkage_matrix, orientation="right", labels=titles);
+
+    plt.tick_params(\
+        axis= 'x',          # changes apply to the x-axis
+        which='both',      # both major and minor ticks are affected
+        bottom='off',      # ticks along the bottom edge are off
+        top='off',         # ticks along the top edge are off
+        labelbottom='off')
+
+    plt.tight_layout() #show plot with tight layout
+
+    #uncomment below to save figure
+    plt.savefig('ward_clusters.png', dpi=200) #save figure as ward_clusters
